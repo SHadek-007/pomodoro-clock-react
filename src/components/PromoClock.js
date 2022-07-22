6817961d522bd349d0d0b2c62b9292ff5ad2a0b9
SADEK HOSSAIN <shadeks16@gmail.com>
2022-07-22 20:28:25 +0600

    setup done

---
+++ src/components/PromoClock.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class PromoClock extends React.Component {
+    // constructor () {
+    //     super();
+
+    //     this.state = {
+
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                <h1 className='text-5xl font-bold'>Promodoro Clock</h1>
+            </div>
+        );
+    }
+};
+
+export default PromoClock;
